2002f88aaeefdfdad74e71eae6b164fac24bf88c
thinhorigami-rio <thinhorigami.rio@gmail.com>
2022-11-20 23:24:27 +0700

    register

--- src/main/java/com/project1/service/implement/RegisterService.java
+++ src/main/java/com/project1/service/implement/RegisterService.java
@@ -39,7 +39,4 @@ public class RegisterService implements IService<Staff>{
     @Override
     public ArrayList<Staff> getAll() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
-    
-}
+    }}
