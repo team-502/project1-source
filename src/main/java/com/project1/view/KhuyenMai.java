4f3b827c2e36b0ad77c5484a521680886bdb0d75
thinhorigami-rio <thinhorigami.rio@gmail.com>
2022-12-06 23:18:59 -0500

    layout fix

--- src/main/java/com/project1/view/KhuyenMai.java
+++ src/main/java/com/project1/view/KhuyenMai.java
@@ -4,17 +4,7 @@
  */
 package com.project1.view;
 
-import com.project1.model.Promotion;
-import com.project1.model.PromotionDetail;
-import com.project1.model_adapter.ProductDetailAdapter;
-import com.project1.model_adapter.promotionAdater;
-import com.project1.repository.implement.ProductDetailReposytory;
-import com.project1.repository.implement.PromotionRepository;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Optional;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import com.project1.model.Promotion;
 import com.project1.model.PromotionDetail;
 import com.project1.model_adapter.ProductDetailAdapter;
@@ -434,40 +424,6 @@ public class KhuyenMai extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_suaActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(KhuyenMai.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(KhuyenMai.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(KhuyenMai.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(KhuyenMai.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new KhuyenMai().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_luu;
